7627b502567341bbadd0db04a01fb55f528eb8a5
Vasyl Kazimirov <vakazimirov@microsoft.com>
2023-09-12 16:01:20 +0200

    Some more refactoring

--- src/Tool/MigratorCommand.cs
+++ src/Tool/MigratorCommand.cs
@@ -117,107 +117,115 @@ namespace Tool
 
             foreach (var projectFile in allProjectFiles)
             {
-                Log.Logger.Information("Reading project {FullName} for PackageReferences", projectFile.FullName);
-                var projectDocument = new XmlDocument();
-                using var fileStream = projectFile.OpenRead();
-                projectDocument.Load(fileStream);
-                Log.Logger.Debug("Project XML loaded");
-
-                Log.Logger.Debug("Checking for legacy XML namespace");
-                var msbuildXmlQueryHelper = new MSBuildXmlNamespaceQueryHelper(projectDocument);
-                var legacyNamespace = msbuildXmlQueryHelper.SelectSingleNode(projectDocument, "Project", true) != null;
-                Log.Logger.Debug("Legacy namespace used {LegacyNamespace}", legacyNamespace);
-
-                Log.Logger.Debug("Checking for PackageReferences");
-                var packageReferences = msbuildXmlQueryHelper.SelectNodes(projectDocument, "//PackageReference", legacyNamespace);
-                Log.Logger.Information("Found {Count} PackageReferences", packageReferences?.Count ?? 0);
-
-                IProjectAnalyzer? project = null;
-                IAnalyzerResult? firstBuildResult = null;
-
-                var packagesInProject = new Dictionary<string, NuGetPackageInfo>();
-                if (packageReferences == null || packageReferences.Count < 1)
+                var packages = ParseProjectFile(settings, projectFile, latestPackage);
+                if (packages != null)
                 {
-                    Log.Logger.Debug("No PackageReferences found in {FullName}", projectFile.FullName);
-                    continue;
-                }
-
-                foreach (XmlNode packageReference in packageReferences)
-                {
-                    Log.Logger.Debug("Checking Include and Version attributes on PackageReference");
-                    var packageName = packageReference.Attributes?["Include"]?.Value;
-                    var packageVersion = packageReference.Attributes?["Version"]?.Value;
-                    if (string.IsNullOrEmpty(packageVersion))
-                    {
-                        Log.Logger.Warning("Project {FullName} contained PackageReferences without Version attributes, Central Package Versioning SDK or Central Package Management could be setup", projectFile.FullName);
-                        continue;
-                    }
-
-                    var variablesToReplace = Regex.Matches(packageVersion, @"\$\(.*\)");
-                    foreach (Match replaceVariable in variablesToReplace)
-                    {
-                        if (firstBuildResult == null)
-                        {
-                            try
-                            {
-                                firstBuildResult = DesignTimeBuildHelper.ExecuteDesignTimeBuild(settings, projectFile, out project);
-                            }
-                            catch (Exception ex)
-                            {
-                                Log.Logger.Error(ex, "Failed to execute design-time build on: {FullName}", projectFile.FullName);
-                                throw;
-                            }
-                        }
-
-                        Log.Logger.Debug("\tResolving MSBuild property: {Value}", replaceVariable.Value);
-                        var propertyName = replaceVariable.Value.Substring(2, replaceVariable.Value.Length - 3); // Skip first 2 chars, extract length minus first 2 chars and last char = 3
-                        var resolvedVariable = firstBuildResult.GetProperty(propertyName);
-                        Log.Logger.Debug("\tResolved {PropertyName} to: {ResolvedVariable}", propertyName, resolvedVariable);
-                        packageVersion = packageVersion.Replace(replaceVariable.Value, resolvedVariable);
-                    }
-
-                    if (packageName == null)
-                    {
-                        Log.Logger.Warning("Null package name or version detected: {PackageReference}", packageReference);
-                        continue;
-                    }
-
-                    var packageInfo = new NuGetPackageInfo(packageName, packageVersion);
-                    Log.Logger.Debug("Nuget package found in {FullName}, package ID {PackageName} and version {PackageVersion}", projectFile.FullName, packageName, packageVersion);
-
-                    packagesInProject.Add(packageName, packageInfo);
-                    Log.Logger.Debug("Package added for lookup inside project");
-
-                    if (latestPackage.ContainsKey(packageName))
-                    {
-                        Log.Logger.Debug("Latest package lookup contains package {PackageName}", packageName);
-                        var latestPackageVersion = latestPackage[packageName];
-                        if (packageInfo.CompareTo(latestPackageVersion) == 1)
-                        {
-                            // package has a higher version then latestPackageVersion
-                            latestPackage[packageName] = packageInfo;
-                            Log.Logger.Debug("Updated latest package version for {PackageName} to version {PackageVersion}", packageName, packageVersion);
-                        }
-                        else
-                        {
-                            Log.Logger.Debug("Package had version equal to or less than latest version");
-                        }
-                    }
-                    else
-                    {
-                        latestPackage.Add(packageName, packageInfo);
-                        Log.Logger.Debug("Added new package {PackageName} as latest package with version {PackageVersion}", packageName, packageVersion);
-                    }
-                }
-
-                if (packagesInProject.Any())
-                {
-                    projectPackageLookup.Add(projectFile, packagesInProject);
                     Log.Logger.Debug("Added project file and packages for lookup on file {FullName}", projectFile.FullName);
+                    projectPackageLookup.Add(projectFile, packages);
                 }
             }
 
             return (latestPackage, projectPackageLookup);
+        }
+
+        private static Dictionary<string, NuGetPackageInfo>? ParseProjectFile(Settings settings, FileInfo projectFile, Dictionary<string, NuGetPackageInfo> latestPackage)
+        {
+            Log.Logger.Information("Reading project {FullName} for PackageReferences", projectFile.FullName);
+            var projectDocument = new XmlDocument();
+            using var fileStream = projectFile.OpenRead();
+            projectDocument.Load(fileStream);
+            Log.Logger.Debug("Project XML loaded");
+
+            Log.Logger.Debug("Checking for legacy XML namespace");
+            var msbuildXmlQueryHelper = new MSBuildXmlNamespaceQueryHelper(projectDocument);
+            var legacyNamespace = msbuildXmlQueryHelper.SelectSingleNode(projectDocument, "Project", true) != null;
+            Log.Logger.Debug("Legacy namespace used {LegacyNamespace}", legacyNamespace);
+
+            Log.Logger.Debug("Checking for PackageReferences");
+            var packageReferences = msbuildXmlQueryHelper.SelectNodes(projectDocument, "//PackageReference", legacyNamespace);
+            Log.Logger.Information("Found {Count} PackageReferences", packageReferences?.Count ?? 0);
+
+            IProjectAnalyzer? project = null;
+            IAnalyzerResult? firstBuildResult = null;
+
+            var packagesInProject = new Dictionary<string, NuGetPackageInfo>();
+            if (packageReferences == null || packageReferences.Count < 1)
+            {
+                Log.Logger.Debug("No PackageReferences found in {FullName}", projectFile.FullName);
+                return null;
+            }
+
+            foreach (XmlNode packageReference in packageReferences)
+            {
+                Log.Logger.Debug("Checking Include and Version attributes on PackageReference");
+                var packageName = packageReference.Attributes?["Include"]?.Value;
+                var packageVersion = packageReference.Attributes?["Version"]?.Value;
+                if (string.IsNullOrEmpty(packageVersion))
+                {
+                    Log.Logger.Warning(
+                        "Project {FullName} contained PackageReferences without Version attributes, Central Package Versioning SDK or Central Package Management could be setup",
+                        projectFile.FullName);
+                    continue;
+                }
+
+                var variablesToReplace = Regex.Matches(packageVersion, @"\$\(.*\)");
+                foreach (Match replaceVariable in variablesToReplace)
+                {
+                    if (firstBuildResult == null)
+                    {
+                        try
+                        {
+                            firstBuildResult = DesignTimeBuildHelper.ExecuteDesignTimeBuild(settings, projectFile, out project);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Logger.Error(ex, "Failed to execute design-time build on: {FullName}", projectFile.FullName);
+                            throw;
+                        }
+                    }
+
+                    Log.Logger.Debug("\tResolving MSBuild property: {Value}", replaceVariable.Value);
+                    var propertyName = replaceVariable.Value.Substring(2, replaceVariable.Value.Length - 3); // Skip first 2 chars, extract length minus first 2 chars and last char = 3
+                    var resolvedVariable = firstBuildResult.GetProperty(propertyName);
+                    Log.Logger.Debug("\tResolved {PropertyName} to: {ResolvedVariable}", propertyName, resolvedVariable);
+                    packageVersion = packageVersion.Replace(replaceVariable.Value, resolvedVariable);
+                }
+
+                if (packageName == null)
+                {
+                    Log.Logger.Warning("Null package name or version detected: {PackageReference}", packageReference);
+                    continue;
+                }
+
+                var packageInfo = new NuGetPackageInfo(packageName, packageVersion);
+                Log.Logger.Debug("Nuget package found in {FullName}, package ID {PackageName} and version {PackageVersion}", projectFile.FullName, packageName, packageVersion);
+
+                packagesInProject.Add(packageName, packageInfo);
+                Log.Logger.Debug("Package added for lookup inside project");
+
+                if (latestPackage.ContainsKey(packageName))
+                {
+                    Log.Logger.Debug("Latest package lookup contains package {PackageName}", packageName);
+                    var latestPackageVersion = latestPackage[packageName];
+                    if (packageInfo.CompareTo(latestPackageVersion) == 1)
+                    {
+                        // package has a higher version then latestPackageVersion
+                        latestPackage[packageName] = packageInfo;
+                        Log.Logger.Debug("Updated latest package version for {PackageName} to version {PackageVersion}", packageName, packageVersion);
+                    }
+                    else
+                    {
+                        Log.Logger.Debug("Package had version equal to or less than latest version");
+                    }
+                }
+                else
+                {
+                    latestPackage.Add(packageName, packageInfo);
+                    Log.Logger.Debug("Added new package {PackageName} as latest package with version {PackageVersion}", packageName, packageVersion);
+                }
+            }
+
+            return packagesInProject.Any() ? packagesInProject : null;
         }
 
         private static void UpdateDirectoryPackagesProps(string directoryPackagesPropsPath, Dictionary<string, NuGetPackageInfo> latestPackageByName)
